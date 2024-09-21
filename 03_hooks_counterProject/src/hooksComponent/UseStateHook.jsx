c2abbcf8c0bcd2fa86001bdebcdc251bd42be2c4
im-sayann <yosayan007@gmail.com>
2024-09-21 08:24:17 +0530

    updated user input

--- 03_hooks_counterProject/src/hooksComponent/UseStateHook.jsx
+++ 03_hooks_counterProject/src/hooksComponent/UseStateHook.jsx
@@ -5,15 +5,18 @@ function UseStateHook() {
   const [input, setInput] = useState("")
   const [text, setText] = useState("")
 
-  const onSubmitClick = () => {
+  const onSubmitClick = (e) => {
+    e.preventDefault()
     setText(input)
   }
   
   return (
     <div>
       <h1>{text}</h1>
-      <input type="text" placeholder='text' onChange={(e) => setInput(e.target.value)} value={input}/>
-      <button onClick={onSubmitClick}>Submit</button>
+      <form onSubmit={onSubmitClick}>
+        <input type="text" placeholder='text' onChange={(e) => setInput(e.target.value)} value={input}/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
